2157e87edfd8328782510e11a1aafa82f309616a
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-04-17 15:03:00 +1000

    pipelines: make namespace mandatory

--- pkg/cmd/pipelines.go
+++ pkg/cmd/pipelines.go
@@ -15,36 +15,27 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+)
+
+var (
+	namespace string
 )
 
 // pipelinesCmd represents the pipelines command
 var pipelinesCmd = &cobra.Command{
 	Use:   "pipelines",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("pipelines called")
-	},
+	Short: "Handles pipelines",
+	Long:  `add long description`, // TODO(sthaha)
 }
 
 func init() {
 	rootCmd.AddCommand(pipelinesCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// pipelinesCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// pipelinesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	pipelinesCmd.PersistentFlags().StringVarP(
+		&namespace,
+		"namespace", "n",
+		"",
+		"namespace to use (mandatory)")
+	pipelinesCmd.MarkPersistentFlagRequired("namespace")
 }
